87890eb776e0f0ba811e9c341e6715c67dee9c88
server <server@sk.com>
2022-09-09 11:58:04 +0800

    fmt

---
+++ format_out/target/debug/deps/format_out-37ac1fc37ed82e17.d
@@ -0,0 +1,5 @@
+d:\code\RustCode\format_out\target\debug\deps\format_out-37ac1fc37ed82e17.rmeta: src\main.rs
+
+d:\code\RustCode\format_out\target\debug\deps\format_out-37ac1fc37ed82e17.d: src\main.rs
+
+src\main.rs:
---
+++ format_out/target/debug/deps/format_out-a6f7983700d70142.d
@@ -0,0 +1,5 @@
+d:\code\RustCode\format_out\target\debug\deps\format_out-a6f7983700d70142.rmeta: src\main.rs
+
+d:\code\RustCode\format_out\target\debug\deps\format_out-a6f7983700d70142.d: src\main.rs
+
+src\main.rs:

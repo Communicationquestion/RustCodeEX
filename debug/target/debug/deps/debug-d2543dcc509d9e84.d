790b7067e99fbc49aa57d68b489413e943cb1ffd
server <server@sk.com>
2022-09-09 12:43:23 +0800

    格式化

---
+++ debug/target/debug/deps/debug-d2543dcc509d9e84.d
@@ -0,0 +1,5 @@
+d:\code\RustCode\debug\target\debug\deps\debug-d2543dcc509d9e84.rmeta: src\main.rs
+
+d:\code\RustCode\debug\target\debug\deps\debug-d2543dcc509d9e84.d: src\main.rs
+
+src\main.rs:
---
+++ debug/target/debug/deps/debug-f04a4dbc1b2e30b7.d
@@ -0,0 +1,5 @@
+d:\code\RustCode\debug\target\debug\deps\debug-f04a4dbc1b2e30b7.rmeta: src\main.rs
+
+d:\code\RustCode\debug\target\debug\deps\debug-f04a4dbc1b2e30b7.d: src\main.rs
+
+src\main.rs:
